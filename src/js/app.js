674458e1f8b1082a4519fbb611c94900f4ef2cd7
Antonny k <akecsc@gmail.com>
2019-06-28 00:59:09 -0500

    Informacion: repositorio ahora es remoto en github-app.js

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+console.log("Ya se conectó con github desde el terminal");
+console.log("Estos cambios serán guardados con la misma secuencia");
+console.log("Primero un <<git add .>> para registrar los cambios en el area temporal");
+console.log('Segundo un <<git commit -m "mensaje">> para registrar los cambios con el commit al repositorio');
+console.log('Tercero un <<git push>> enviar los cambios al repositorio remoto');
