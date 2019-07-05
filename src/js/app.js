0cb328fbf1456aefdacf85e00a5d2e0870b126c8
llkiriell <akecsc@gmail.com>
2019-07-04 22:42:03 -0500

    Se agrego los titulos para la version siguiente

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,4 @@ console.log("Estos cambios serán guardados con la misma secuencia");
 console.log("Primero un <<git add .>> para registrar los cambios en el area temporal");
 console.log('Segundo un <<git commit -m "mensaje">> para registrar los cambios con el commit al repositorio');
 console.log('Tercero un <<git push>> enviar los cambios al repositorio remoto');
+console.log('Línea para la versión 1.1');
